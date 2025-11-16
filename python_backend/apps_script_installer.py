9bbe9552418dda770a7d2f7ff52a6de3cce17ad4
Konsta Varonen <konsta@finata.ai>
2025-11-16 01:17:29 +0100

    fix: allow service account to read shared sheets

--- python_backend/apps_script_installer.py
+++ python_backend/apps_script_installer.py
@@ -34,7 +34,7 @@ class AppsScriptInstaller:
         """
         scopes = [
             "https://www.googleapis.com/auth/script.projects",
-            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive",
             "https://www.googleapis.com/auth/spreadsheets",
         ]
 
